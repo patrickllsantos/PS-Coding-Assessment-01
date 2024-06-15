e3f8e771c4817b20d282889050bf730dcc123f6a
Patrick Santos <santos.patrick.lloyd@gmail.com>
2024-06-15 20:04:21 +0800

    feat(api): add PizzaTypes controller and import handler

---
+++ CodingAssessment/Features/PizzaTypes/ImportPizzaTypes.cs
@@ -0,0 +1,97 @@
+﻿using System.Globalization;
+using CodingAssessment.Database;
+using CodingAssessment.Exceptions;
+using CodingAssessment.Mapping.CsvMapping;
+using CodingAssessment.Models;
+using CodingAssessment.Utilities;
+using CsvHelper;
+using CsvHelper.Configuration;
+using FluentValidation;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using ValidationException = CodingAssessment.Exceptions.ValidationException;
+
+namespace CodingAssessment.Features.PizzaTypes;
+
+public static class ImportPizzaTypes
+{
+    public record Command(Request Request) : IRequest<Unit>;
+    public record Request(IFormFile File);
+
+    public class Validation : AbstractValidator<Request>
+    {
+        public Validation()
+        {
+            RuleFor(x => x.File)
+                .NotNull().WithMessage("File is required.");
+            
+            RuleFor(x => x.File)
+                .Must(ValidationHelpers.BeAValidCsv).WithMessage("Only CSV files are allowed.");
+        }
+    }
+    
+    internal sealed class Handler : IRequestHandler<Command, Unit>
+    {
+        private readonly DatabaseContext _context;
+
+        public Handler(DatabaseContext context)
+        {
+            _context = context;
+        }
+        
+        public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
+        {
+            var validation = await new Validation().ValidateAsync(request.Request, cancellationToken);
+
+            if (!validation.IsValid)
+            {
+                var errorMessages = validation.Errors.Select(e => e.ErrorMessage);
+                throw new ValidationException(errorMessages);
+            }
+
+            try
+            {
+                using var reader = new StreamReader(request.Request.File.OpenReadStream());
+                using var csv = new CsvReader(reader, new CsvConfiguration(CultureInfo.InvariantCulture)
+                {
+                    HeaderValidated = null,
+                    MissingFieldFound = null
+                });
+
+                var categories = await _context.PizzaCategories.ToDictionaryAsync(
+                    c => c.Name,
+                    c => c.Id,
+                    StringComparer.OrdinalIgnoreCase,
+                    cancellationToken);
+
+                csv.Context.RegisterClassMap(new PizzaTypeMap(categories));
+
+                var pizzaTypes = csv.GetRecords<PizzaType>().ToList();
+                
+                if (pizzaTypes.Count is not 0)
+                {
+                    try
+                    {
+                        _context.PizzaTypes.AddRange(pizzaTypes);
+                        await _context.SaveChangesAsync(cancellationToken);
+                    }
+                    catch (DbUpdateException ex) when (DatabaseExceptionHelper.IsDuplicateKeyException(ex))
+                    {
+                        throw new DuplicateKeyException(ErrorMessages.DuplicatePizzaTypeError, ex);
+                    }
+                }
+
+                return Unit.Value;
+            }
+            catch (ReaderException ex)
+            {
+                if (ex.InnerException is CategoryNotFoundException categoryNotFoundException)
+                {
+                    throw new CsvProcessingException(categoryNotFoundException.Message, categoryNotFoundException);
+                }
+                
+                throw new CsvProcessingException(ErrorMessages.CsvProcessingError, ex);
+            }        
+        }        
+    }
+}
---
+++ CodingAssessment/Features/PizzaTypes/PizzaTypesController.cs
@@ -0,0 +1,24 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CodingAssessment.Features.PizzaTypes;
+
+[Controller]
+[Route("api/[controller]")]
+public class PizzaTypesController : ControllerBase
+{
+    private readonly ISender _mediator;
+
+    public PizzaTypesController(ISender mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpPost("import")]
+    public async Task<IActionResult> Import(ImportPizzaTypes.Request request, CancellationToken cancellationToken)
+    {
+        var command = new ImportPizzaTypes.Command(request);
+        await _mediator.Send(command, cancellationToken);
+        return Ok();
+    }
+}
