211a052c796618a0bdcb61a5ae22563a4c93ec6d
Patrick Santos <santos.patrick.lloyd@gmail.com>
2024-06-15 14:03:40 +0800

    feat(models): add pizza and order related models

---
+++ CodingAssessment/Models/Order.cs
@@ -0,0 +1,30 @@
+﻿namespace CodingAssessment.Models;
+
+/// <summary>
+/// Represents an order placed by a customer.
+/// </summary>
+public class Order
+{
+    /// <summary>
+    /// Gets or sets the unique identifier for the order.
+    /// </summary>
+    public int Id { get; set; }
+
+    /// <summary>
+    /// Gets or sets the date the order was placed.
+    /// </summary>
+    public DateTime Date { get; set; }
+
+    /// <summary>
+    /// Gets or sets the time the order was placed.
+    /// </summary>
+    public TimeSpan Time { get; set; }
+
+    /// <summary>
+    /// Gets or sets the details of the order.
+    /// </summary>
+    /// <remarks>
+    /// Navigation property for the details of the order.
+    /// </remarks>
+    public ICollection<OrderDetails> OrderDetails { get; set; } = new List<OrderDetails>();
+}   
---
+++ CodingAssessment/Models/OrderDetails.cs
@@ -0,0 +1,43 @@
+﻿namespace CodingAssessment.Models;
+
+/// <summary>
+/// Represents the details of the placed order by a customer
+/// </summary>
+public class OrderDetails
+{
+    /// <summary>
+    /// Gets or sets the unique identifier for the order details
+    /// </summary>
+    public int Id { get; set; }
+
+    /// <summary>
+    /// Gets or sets the order associated with the order details.
+    /// </summary>
+    /// <remarks>
+    /// Navigation property for the order associated with the order details.
+    /// </remarks>
+    public Order Order { get; set; }
+
+    /// <summary>
+    /// Gets or sets the order id
+    /// </summary>
+    public required int OrderId { get; set; }
+
+    /// <summary>
+    /// Gets or sets the pizza associated with the order details.
+    /// </summary>
+    /// <remarks>
+    /// Navigation property for the pizza associated with the order details.
+    /// </remarks>
+    public Pizza Pizza { get; set; }
+
+    /// <summary>
+    /// Gets or sets the pizza id
+    /// </summary>
+    public required string PizzaId { get; set; }
+
+    /// <summary>
+    /// Gets or sets the quantity
+    /// </summary>
+    public required int Quantity { get; set; }
+}
